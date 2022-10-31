72e498cddaa6752f882a6fd1a194bf3159c56e10
Денис Нестеров <iwebdroid@yandex.ru>
2022-10-31 14:53:11 +0300

    redoc url update

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,6 +3,11 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
     path('api/', include('users.urls')),
     path('api/', include('api.urls')),
 ]
